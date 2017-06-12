626bcb047d2368aa49b72a30cc9e704d5d26a264
Cole Meurer <cole.meurer@gmail.com>
2017-06-12 16:58:27 +0900

    ability to add recipe

--- app.rb
+++ app.rb
@@ -23,6 +23,21 @@ get '/' do
   erb :index
 end
 
+# Adding a new recipe
+get '/recipes/add' do
+  erb :add_form
+end
+
+post '/add' do
+  name = params[:name]
+  description = params[:description]
+  difficulty = params[:difficulty].to_i if params[:difficulty]
+  prep_time = "#{params[:prep_time]} min" if params[:prep_time]
+  cooking_time = "#{params[:cooking_time]} min" if params[:cooking_time]
+  $cookbook.add_recipe(Recipe.new(name, description, difficulty, prep_time, cooking_time))
+  redirect '/'
+end
+
 # Deleting a recipe
 get '/recipes/delete' do
   @recipes = View.list($cookbook.all)
